52c7381a9ded64f9a229f148603fab73a89b17d9
marclmao <119444616+marclmao@users.noreply.github.com>
2023-12-05 13:07:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]args){
+        for (int x = 100; x >=1 ; x--) {
+            System.out.println(x + " bottles of beer on the wall");
+            System.out.println(x + " bottles of beer");
+            System.out.println("Take one down and pass it around");
+            System.out.println(x + " bottles of beer on the wall");
+        }
+    }
+}
